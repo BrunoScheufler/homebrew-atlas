0123d5cbce4541ba4148d899862cb0d212b2870f
Bruno Scheufler <git@brunoscheufler.com>
2022-09-19 11:06:33 +0200

    initial commit

---
+++ Formula/atlas.rb
@@ -0,0 +1,34 @@
+# Documentation: https://docs.brew.sh/Formula-Cookbook
+#                https://rubydoc.brew.sh/Formula
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+class Atlas < Formula
+  desc "improved local development"
+  homepage "https://github.com/BrunoScheufler/atlas"
+  url "https://github.com/BrunoScheufler/atlas/archive/v0.0.1-alpha.1.tar.gz"
+  version "0.0.1-alpha.1"
+  sha256 "cac6d7fba77d22d56c34b6c106dc917c375185ae11e810edee0d68aa62805976"
+  license "MIT"
+
+  depends_on "go" => :build
+
+  def install
+    # ENV.deparallelize  # if your formula fails when building in parallel
+    system "go", "build", *std_go_args(ldflags: "-s -w"), "./cli"
+    # would also work:
+    # system "go", "build", "-o", "bin/cli", "./cli"
+    # bin.install "bin/cli" => "atlas"
+  end
+
+  test do
+    # `test do` will create, run in and delete a temporary directory.
+    #
+    # This test will fail and we won't accept that! For Homebrew/homebrew-core
+    # this will need to be a test that verifies the functionality of the
+    # software. Run the test with `brew test atlas`. Options passed
+    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
+    #
+    # The installed folder is not in the path, so use the entire path to any
+    # executables being tested: `system "#{bin}/program", "do", "something"`.
+    system "false"
+  end
+end
---
+++ README.md
@@ -0,0 +1,11 @@
+# Atlas Homebrew Tap
+
+## getting started
+
+Run the following to add the tap and install the latest version of [Atlas](https://github.com/brunoscheufler/atlas)
+
+```bash
+brew tap brunoscheufler/atlas
+
+brew install brunoscheufler/atlas/atlas
+```
