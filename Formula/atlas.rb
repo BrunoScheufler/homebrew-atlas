e54fc317618bd6c0b647d2b7c9be06c5e93b01d2
Bruno Scheufler <git@brunoscheufler.com>
2022-10-05 10:20:31 +0200

    atlas 0.0.1-alpha.9 (#9)
    Created by https://github.com/mislav/bump-homebrew-formula-action

--- Formula/atlas.rb
+++ Formula/atlas.rb
@@ -4,9 +4,9 @@
 class Atlas < Formula
   desc "improved local development"
   homepage "https://github.com/BrunoScheufler/atlas"
-  url "https://github.com/BrunoScheufler/atlas/archive/v0.0.1-alpha.8.tar.gz"
-  version "0.0.1-alpha.8"
-  sha256 "963a173f6e19cf79bf61300f56977193e50e596f865605aed1896e32b2697dc8"
+  url "https://github.com/BrunoScheufler/atlas/archive/v0.0.1-alpha.9.tar.gz"
+  version "0.0.1-alpha.9"
+  sha256 "90b2aa47eb7e45282dd428607a60617e30ab066cd202a940dfb37956dd19add6"
   license "MIT"
 
   depends_on "go" => :build
